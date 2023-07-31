0ba9bfd20664617afaa72f6aad2c34a2b69ba4ac
Перминов Сергей Олегович <S_Perminov@bz.ru>
2023-07-31 14:29:19 +0300

    ready_4

--- 4_pet_registry_project_part/Pet-registry/src/main/java/org/gbtask/controller/AppController.java
+++ 4_pet_registry_project_part/Pet-registry/src/main/java/org/gbtask/controller/AppController.java
@@ -2,11 +2,8 @@ package org.gbtask.controller;
 
 import org.gbtask.view.menu.impl.GeneralMenuImpl;
 
-import java.io.IOException;
-import java.sql.*;
-
 public class AppController {
-    public static void start(){
+    public static void start() {
         new GeneralMenuImpl().processing();
     }
 }
